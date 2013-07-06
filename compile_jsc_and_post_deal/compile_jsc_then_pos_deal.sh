85aca38cc6af81443d8a6966819b42b6b105277c
Yuechuzhao <oisiiryouri@outlook.com>
2013-07-06 10:37:34 +0800

    change to absolute path

--- compile_jsc_and_post_deal/compile_jsc_then_pos_deal.sh
+++ compile_jsc_and_post_deal/compile_jsc_then_pos_deal.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 echo "Start compile and post deal"
-python ${PWD}/compile_jsc.py -c $1
+script_path=`dirname "$0"`
+python ${script_path}/compile_jsc.py -c $1
 echo "-----------------"
 echo "compile completed!"
 echo "-----------------"
-python ${PWD}/post_deal.py -d $1
+python ${script_path}/post_deal.py -d $1
 echo "-----------------"
 echo "post deal completed!"
 echo "-----------------"
